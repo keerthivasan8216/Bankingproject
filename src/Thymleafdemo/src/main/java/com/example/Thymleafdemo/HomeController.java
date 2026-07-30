117d85e3afae89f139f11e89f1b826d330326479
keerthivasan8216 <122586791+keerthivasan8216@users.noreply.github.com>
2026-07-30 11:50:52 +0530

    Add files via upload

---
+++ src/Thymleafdemo/src/main/java/com/example/Thymleafdemo/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.Thymleafdemo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        model.addAttribute("name", "John");
+        model.addAttribute("city", "Bangalore");
+
+        return "home";
+    }
+}
+
